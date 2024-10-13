52504f9d69ed0e81f8b59354d94f3fc75a437e00
raj-ranjan-70 <raj.ranjan9178@gmail.com>
2024-10-13 18:57:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example to show interaction
+document.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        alert('You clicked on a link!');
+    });
+});
